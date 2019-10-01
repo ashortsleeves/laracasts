3a46488068597058e2a118c485605c58d2610f60
Adam Shortsleeves <ashortsleeves@gmail.com>
2019-10-01 11:08:13 -0400

    section 3 lesson 10

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,12 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $name = request('name');
+
+    return view('test', [
+      'name' => $name
+    ]);
 });
+
+Route::get('/posts/{post}','PostsController@show');
